e2d34fd78bde999953be9e6e8d0cd6bc9158bee1
AndreyShal <93115383+AndreyShal@users.noreply.github.com>
2021-11-19 13:30:21 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict'
+
+document.getElementById('click-to-hide').addEventListener('click', hiddenCloseclick);
+function hiddenCloseclick() {
+    let x = document.getElementById("faceID");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
