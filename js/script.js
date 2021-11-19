b68626394d03acf7e7f1ab66d0dfe4d7a41af08a
AndreyShal <93115383+AndreyShal@users.noreply.github.com>
2021-11-19 22:08:27 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 'use strict'
 
-document.getElementById('click-to-hide').addEventListener('click', hiddenCloseclick);
-function hiddenCloseclick() {
-    let x = document.getElementById("faceID");
-    if (x.style.display == "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none"
-    }
-}
+// document.getElementById('click-to-hide').addEventListener('click', hiddenCloseclick);
+// function hiddenCloseclick() {
+//     let x = document.getElementById("faceID");
+//     if (x.style.display == "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none"
+//     }
+// }
 
 
 document.querySelector(".buttonTop").addEventListener('click', function () {
