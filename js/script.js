6b41cbfab2e57113f335cb71b15d865467eab3b5
AndreyShal <93115383+AndreyShal@users.noreply.github.com>
2021-11-24 12:45:31 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 'use strict'
 
-// document.getElementById('click-to-hide').addEventListener('click', hiddenCloseclick);
-// function hiddenCloseclick() {
-//     let x = document.getElementById("faceID");
-//     if (x.style.display == "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none"
-//     }
-// }
-
-
 document.querySelector(".buttonTop").addEventListener('click', function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
